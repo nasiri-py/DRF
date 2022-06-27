1bf764d6fa4dfa282e2cc2fdd6365fa5b9622fb4
sinanasiripy <sinanasiri.py@gmail.com>
2022-06-27 20:59:26 +0430

    add routers

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,21 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
 
 
 app_name = 'api'
+# urlpatterns = [
+#     path('', views.ArticleList.as_view(), name='list'),
+#     path('<int:pk>/', views.ArticleDetail.as_view(), name='detail'),
+#     path('users/', views.UserList.as_view(), name='user_list'),
+#     path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+# ]
+
+router = routers.SimpleRouter()
+router.register('', views.ArticleViewSet)
+router.register('users', views.UserViewSet)
+
+# urlpatterns = router.urls
 urlpatterns = [
-    path('', views.ArticleList.as_view(), name='list'),
-    path('<int:pk>/', views.ArticleDetail.as_view(), name='detail'),
-    path('users/', views.UserList.as_view(), name='user_list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+    path("", include(router.urls))
 ]
