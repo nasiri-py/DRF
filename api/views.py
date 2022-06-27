17968b31857680ff12d76c8f3bc98338bfec30f1
sinanasiripy <sinanasiri.py@gmail.com>
2022-06-27 15:32:30 +0430

    add ViewSets

--- api/views.py
+++ api/views.py
@@ -1,32 +1,51 @@
 from .serializers import ArticleSerializer, UserSerializer
 from blogs.models import Article
 from django.contrib.auth.models import User
-from rest_framework import generics
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from .permissions import IsSuperUser, IsAuthorOrReadOnly, IsSuperUserOrStaffReadOnly
+# from rest_framework import generics
+# from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from .permissions import IsSuperUser, IsAuthorOrReadOnly, IsSuperUserOrStaffReadOnly, IsStaffOrReadOnly
 # from rest_framework.views import APIView
 # from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 
-class ArticleList(generics.ListCreateAPIView):
+# class ArticleList(generics.ListCreateAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#
+#
+# class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+#     # lookup_field = 'slug'
+#     permission_classes = [IsAuthorOrReadOnly]
+
+
+class ArticleViewSet(ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-
-class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-    # lookup_field = 'slug'
-    permission_classes = [IsAuthorOrReadOnly]
-
-
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsSuperUserOrStaffReadOnly]
+    def get_permissions(self):
+        if self.action == 'list':
+            permission_classes = [IsStaffOrReadOnly]
+        else:
+            permission_classes = [IsStaffOrReadOnly, IsAuthorOrReadOnly]
+        return [permission() for permission in permission_classes]
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+# class UserList(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     permission_classes = [IsSuperUserOrStaffReadOnly]
+#
+#
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     permission_classes = [IsSuperUserOrStaffReadOnly]
+
+
+class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsSuperUserOrStaffReadOnly]
