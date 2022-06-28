60ad4ec87ecbe3836c192b453207415243876a6a
sinanasiripy <sinanasiri.py@gmail.com>
2022-06-28 20:48:46 +0430

    nasted serializers

--- api/serializers.py
+++ api/serializers.py
@@ -4,7 +4,14 @@ from blogs.models import Article
 from django.contrib.auth import get_user_model
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'first_name', 'last_name']
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
     class Meta:
         model = Article
         # fields = ['author', 'title', 'slug', 'publish', 'text', 'status']
